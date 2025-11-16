61306c31eea2f900bae396136c5b9111625931ee
Uchiha711 <clashclans7112016@gmail.com>
2025-11-16 21:28:05 +0530

    push deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WEBROOT=~/domains/test.goldgladiators.com/public_html/test
+
+echo "Pulling latest changes..."
+git pull origin main
+
+echo "Installing dependencies..."
+npm install --force
+
+echo "Building project..."
+npm run build
+
+echo "Deploying files to public_html..."
+rm -rf $WEBROOT/*
+cp -R dist/* $WEBROOT/
+
+echo "Deployment complete!"
